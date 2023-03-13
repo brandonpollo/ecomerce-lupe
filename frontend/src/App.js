222d5ba4058a6090020f3f5f2a23b75157649783
admin <correo@gmail.com>
2023-03-13 08:05:40 -0600

    lupe-01

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <a href="/">Lupe</a>
+      </header>
+      <main>
+        lista de productos
+      </main>
+    </div>
+  );
+}
+
+export default App;
